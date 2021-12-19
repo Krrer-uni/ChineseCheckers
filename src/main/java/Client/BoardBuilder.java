e738109d86bb844f4ba190da1a379ab09e3f10e2
Krrer-uni <JaramBlanta420XD>
2021-12-19 12:53:47 +0100

    Game client with working movement.

--- src/main/java/Client/BoardBuilder.java
+++ src/main/java/Client/BoardBuilder.java
@@ -14,25 +14,33 @@ public class BoardBuilder {
     int playerCount;
     private int playerId;
     private Dimension windowDimension;
-    private int fieldDistance;
+    private final int fieldDistance;
     private int emptyFieldDistanceCorrection;
     private Dimension emptyFieldDimension;
-    private Dimension nonEmptyFieldDimension;
+    private final Dimension nonEmptyFieldDimension;
+
+    private final int xBoardOffset;
     private ArrayList<ArrayList<Integer>> layout;
 
     public BoardBuilder(Dimension windowDimension) {
         this.windowDimension = windowDimension;
 
+
         rows = 17;
         columns = 13;
+
         layout = new ArrayList<>();
-        fieldDistance = windowDimension.width / rows;
-        nonEmptyFieldDimension = new Dimension(windowDimension.width / (2 * rows),
+        fieldDistance = windowDimension.height / (rows +1 ) ;
+
+        nonEmptyFieldDimension = new Dimension(windowDimension.height / (2 * rows),
                 windowDimension.height / (2 * rows));
 
         emptyFieldDistanceCorrection = nonEmptyFieldDimension.width / 2;
-        emptyFieldDimension = new Dimension(windowDimension.width / (2 * rows) / 2,
+
+        emptyFieldDimension = new Dimension(windowDimension.height / (2 * rows) / 2,
                 windowDimension.height / (2 * rows) / 2);
+
+        xBoardOffset = (rows - columns)/2 * fieldDistance;
     }
 
 
@@ -41,20 +49,19 @@ public class BoardBuilder {
         for (int i = 0; i < rows; i++) {
             board.fieldArray.add(new ArrayList<>());
             for (int j = 0; j < columns; j++) {
+                int fieldDistanceTemp = fieldDistance * j + (i % 2 == 1 ?
+                        fieldDistance / 2 : 0) + xBoardOffset;
                 if (layout.get(i).get(j) == 0) {
                     board.fieldArray.get(i).add(new PlayerField(0,
-                            new Point2D.Float(fieldDistance * j + (i % 2 == 1 ?
-                                    fieldDistance / 2 : 0), fieldDistance * i),
+                            new Point2D.Float(fieldDistanceTemp, fieldDistance * i),
                             nonEmptyFieldDimension));
                 } else if (layout.get(i).get(j) == 1) {
                     board.fieldArray.get(i).add(new PlayerField(1,
-                            new Point2D.Float(fieldDistance * j + (i % 2 == 1 ?
-                                    fieldDistance / 2 : 0), fieldDistance * i),
+                            new Point2D.Float(fieldDistanceTemp, fieldDistance * i),
                             nonEmptyFieldDimension));
                 } else if (layout.get(i).get(j) == 2) {
                     board.fieldArray.get(i).add(new PlayerField(2,
-                            new Point2D.Float(fieldDistance * j + (i % 2 == 1 ?
-                                    fieldDistance / 2 : 0), fieldDistance * i),
+                            new Point2D.Float(fieldDistanceTemp, fieldDistance * i),
                             nonEmptyFieldDimension));
                 }
             }
@@ -64,6 +71,7 @@ public class BoardBuilder {
 
 
     public void setLayout(int playerCount) {
+        layout = new ArrayList<>();
         if (playerCount == 2) {
             layout.add(new ArrayList<>(Arrays.asList(
                     -1, -1, -1, -1, -1, -1, 1, -1, -1, -1, -1, -1, -1
--- src/main/java/Client/GamePanel.java
+++ src/main/java/Client/GamePanel.java
@@ -4,8 +4,6 @@ import javax.swing.*;
 import javax.swing.event.MouseInputAdapter;
 import java.awt.*;
 import java.awt.event.MouseEvent;
-import java.awt.geom.Dimension2D;
-import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
 import java.util.ArrayList;
 
@@ -14,10 +12,11 @@ public class GamePanel extends JPanel {
     BoardBuilder boardBuilder;
 
     public GamePanel(int windowWidth, int windowHeight) {
+
         boardBuilder = new BoardBuilder(new Dimension(windowWidth, windowHeight));
         boardBuilder.setLayout(2);
         board = boardBuilder.getBoard();
-        setBackground(new Color(199, 133, 26));
+        setBackground(new Color(160, 101, 28));
         repaint();
         addMouseListener(new MyMouseListener());
     }
@@ -29,7 +28,7 @@ public class GamePanel extends JPanel {
         graphics2D.setPaint(Color.BLUE);
         for (ArrayList<Field> row : board.fieldArray) {
             for (Field field : row) {
-                if (field.getOwnerId() == 0) graphics2D.setPaint(new Color(128, 86, 19));
+                if (field.getOwnerId() == 0) graphics2D.setPaint(new Color(132, 88, 17));
                 else if (field.getOwnerId() == 1) graphics2D.setPaint(Color.BLUE);
                 else if (field.getOwnerId() == 2) graphics2D.setPaint(Color.RED);
                 else if (field.getOwnerId() == 3) graphics2D.setPaint(Color.GREEN);
@@ -41,10 +40,21 @@ public class GamePanel extends JPanel {
         }
     }
 
+    private void updateBoard(int rowSource, int columnSource, int rowTarget, int columnTarget){
+        Rectangle2D tempFrameSource = board.fieldArray.get(rowSource).get(columnSource).getField().getFrame();
+        Rectangle2D tempFrameTarget = board.fieldArray.get(rowTarget).get(columnTarget).getField().getFrame();
+        board.fieldArray.get(rowSource).get(columnSource).getField().setFrame(tempFrameTarget);
+        board.fieldArray.get(rowTarget).get(columnTarget).getField().setFrame(tempFrameSource);
+
+        Field tempField = board.fieldArray.get(rowSource).get(columnSource);
+        board.fieldArray.get(rowSource).set(columnSource, board.fieldArray.get(rowTarget).get(columnTarget));
+        board.fieldArray.get(rowTarget).set(columnTarget, tempField);
+    }
+
     private class MyMouseListener extends MouseInputAdapter {
 
-        int chosenColumn;
-        int chosenRow;
+        int sourceColumn;
+        int sourceRow;
 
         boolean choosingTarget = true;
 
@@ -55,8 +65,8 @@ public class GamePanel extends JPanel {
                     for (Field field : row) {
                         if (field.getField().contains(e.getX(), e.getY())) {
                             System.out.println("wybrano " + row.indexOf(field) + "," + board.fieldArray.indexOf(row));
-                            chosenColumn = row.indexOf(field);
-                            chosenRow = board.fieldArray.indexOf(row);
+                            sourceColumn = row.indexOf(field);
+                            sourceRow = board.fieldArray.indexOf(row);
                             choosingTarget = false;
                             break;
                         }
@@ -68,28 +78,28 @@ public class GamePanel extends JPanel {
                         System.out.println("zamieniono z " + row.indexOf(field) + "," + board.fieldArray.indexOf(row));
                         int targetColumn = row.indexOf(field);
                         int targetRow = board.fieldArray.indexOf(row);
-
-                        Rectangle2D tempBounds = field.getField().getFrame();
-                        double x,y,w,h;
-                        x = tempBounds.getX();
-                        y = tempBounds.getY();
-                        w = tempBounds.getWidth();
-                        h = tempBounds.getHeight();
-
-                        Field tempField = board.fieldArray.get(chosenRow).get(chosenColumn);
-
-                        Rectangle2D tempBounds2 = tempField.getField().getFrame();
-                        double x2,y2,w2,h2;
-                        x2 = tempBounds.getX();
-                        y2 = tempBounds.getY();
-                        w2 = tempBounds.getWidth();
-                        h2 = tempBounds.getHeight();
-
+//
+//                        Rectangle2D tempBounds = field.getField().getFrame();
+//                        double x,y,w,h;
+//                        x = tempBounds.getX();
+//                        y = tempBounds.getY();
+//                        w = tempBounds.getWidth();
+//                        h = tempBounds.getHeight();
+//
 //                        Field tempField = board.fieldArray.get(chosenRow).get(chosenColumn);
-                        board.fieldArray.get(targetRow).get(targetColumn).getField().setFrame(x2,y2,w2,h2);
-//                        field.getField()
-                        board.fieldArray.get(chosenRow).get(chosenColumn).getField().setFrame(x,y,w,h);
-
+//
+//                        Rectangle2D tempBounds2 = tempField.getField().getFrame();
+//                        double x2,y2,w2,h2;
+//                        x2 = tempBounds.getX();
+//                        y2 = tempBounds.getY();
+//                        w2 = tempBounds.getWidth();
+//                        h2 = tempBounds.getHeight();
+//
+////                        Field tempField = board.fieldArray.get(chosenRow).get(chosenColumn);
+//                        board.fieldArray.get(targetRow).get(targetColumn).getField().setFrame(x2,y2,w2,h2);
+////                        field.getField()
+//                        board.fieldArray.get(chosenRow).get(chosenColumn).getField().setFrame(x,y,w,h);
+                        updateBoard(sourceRow,sourceColumn,targetRow,targetColumn);
 
 
 
