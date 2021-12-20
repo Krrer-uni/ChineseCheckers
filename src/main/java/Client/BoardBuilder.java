3f7399291d67998d01a9bcf5191b1560a4894b6e
Krrer-uni <JaramBlanta420XD>
2021-12-20 18:24:56 +0100

    Boards for 3,4,6 players added

--- src/main/java/Client/BoardBuilder.java
+++ src/main/java/Client/BoardBuilder.java
@@ -67,6 +67,18 @@ public class BoardBuilder {
                     board.fieldArray.get(i).add(new PlayerField(3,
                             new Point2D.Float(fieldDistanceTemp, fieldDistance * i),
                             nonEmptyFieldDimension));
+                }else if (layout.get(i).get(j) == 4) {
+                    board.fieldArray.get(i).add(new PlayerField(4,
+                            new Point2D.Float(fieldDistanceTemp, fieldDistance * i),
+                            nonEmptyFieldDimension));
+                }else if (layout.get(i).get(j) == 5) {
+                    board.fieldArray.get(i).add(new PlayerField(5,
+                            new Point2D.Float(fieldDistanceTemp, fieldDistance * i),
+                            nonEmptyFieldDimension));
+                }else if (layout.get(i).get(j) == 6) {
+                    board.fieldArray.get(i).add(new PlayerField(6,
+                            new Point2D.Float(fieldDistanceTemp, fieldDistance * i),
+                            nonEmptyFieldDimension));
                 }
             }
         }
@@ -130,31 +142,31 @@ public class BoardBuilder {
             )));
         } else if (playerCount == 3) {
             layout.add(new ArrayList<>(Arrays.asList(
-                    -1, -1, -1, -1, -1, -1, 1, -1, -1, -1, -1, -1, -1
+                    -1, -1, -1, -1, -1, -1, 0, -1, -1, -1, -1, -1, -1
             )));
             layout.add(new ArrayList<>(Arrays.asList(
-                    -1, -1, -1, -1, -1, 1, 1, -1, -1, -1, -1, -1, -1
+                    -1, -1, -1, -1, -1, 0, 0, -1, -1, -1, -1, -1, -1
             )));
             layout.add(new ArrayList<>(Arrays.asList(
-                    -1, -1, -1, -1, -1, 1, 1, 1, -1, -1, -1, -1, -1
+                    -1, -1, -1, -1, -1, 0, 0, 0, -1, -1, -1, -1, -1
             )));
             layout.add(new ArrayList<>(Arrays.asList(
-                    -1, -1, -1, -1, 1, 1, 1, 1, -1, -1, -1, -1, -1
+                    -1, -1, -1, -1, 0, 0, 0, 0, -1, -1, -1, -1, -1
             )));
             layout.add(new ArrayList<>(Arrays.asList(
-                    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+                    1, 1, 1, 1, 0, 0, 0, 0, 0, 3, 3, 3, 3
             )));
             layout.add(new ArrayList<>(Arrays.asList(
-                    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1
+                    1, 1, 1, 0, 0, 0, 0, 0, 0, 3, 3, 3, -1
             )));
             layout.add(new ArrayList<>(Arrays.asList(
-                    -1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1
+                    -1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 3, 3, -1
             )));
             layout.add(new ArrayList<>(Arrays.asList(
-                    -1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1, -1
+                    -1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 3, -1, -1
             )));
             layout.add(new ArrayList<>(Arrays.asList(
-                    -1, -1, 0, 0, 0, 3, 3, 3, 3, 0, 0, -1, -1
+                    -1, -1, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1, -1
             )));
             layout.add(new ArrayList<>(Arrays.asList(
                     -1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1, -1
@@ -179,6 +191,162 @@ public class BoardBuilder {
             )));
             layout.add(new ArrayList<>(Arrays.asList(
                     -1, -1, -1, -1, -1, -1, 2, -1, -1, -1, -1, -1, -1
+            )));
+        } else if (playerCount == 4) {
+            layout.add(new ArrayList<>(Arrays.asList(
+                    -1, -1, -1, -1, -1, -1, 0, -1, -1, -1, -1, -1, -1
+            )));
+            layout.add(new ArrayList<>(Arrays.asList(
+                    -1, -1, -1, -1, -1, 0, 0, -1, -1, -1, -1, -1, -1
+            )));
+            layout.add(new ArrayList<>(Arrays.asList(
+                    -1, -1, -1, -1, -1, 0, 0, 0, -1, -1, -1, -1, -1
+            )));
+            layout.add(new ArrayList<>(Arrays.asList(
+                    -1, -1, -1, -1, 0, 0, 0, 0, -1, -1, -1, -1, -1
+            )));
+            layout.add(new ArrayList<>(Arrays.asList(
+                    1, 1, 1, 1, 0, 0, 0, 0, 0, 4, 4, 4, 4
+            )));
+            layout.add(new ArrayList<>(Arrays.asList(
+                    1, 1, 1, 0, 0, 0, 0, 0, 0, 4, 4, 4, -1
+            )));
+            layout.add(new ArrayList<>(Arrays.asList(
+                    -1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 4, 4, -1
+            )));
+            layout.add(new ArrayList<>(Arrays.asList(
+                    -1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 4, -1, -1
+            )));
+            layout.add(new ArrayList<>(Arrays.asList(
+                    -1, -1, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1, -1
+            )));
+            layout.add(new ArrayList<>(Arrays.asList(
+                    -1, 2, 0, 0, 0, 0, 0, 0, 0, 0, 3, -1, -1
+            )));
+            layout.add(new ArrayList<>(Arrays.asList(
+                    -1, 2, 2, 0, 0, 0, 0, 0, 0, 0, 3, 3, -1
+            )));
+            layout.add(new ArrayList<>(Arrays.asList(
+                    2, 2, 2, 0, 0, 0, 0, 0, 0, 3, 3, 3, -1
+            )));
+            layout.add(new ArrayList<>(Arrays.asList(
+                    2, 2, 2, 2, 0, 0, 0, 0, 0, 3, 3, 3, 3
+            )));
+            layout.add(new ArrayList<>(Arrays.asList(
+                    -1, -1, -1, -1, 0, 0, 0, 0, -1, -1, -1, -1, -1
+            )));
+            layout.add(new ArrayList<>(Arrays.asList(
+                    -1, -1, -1, -1, -1, 0, 0, 0, -1, -1, -1, -1, -1
+            )));
+            layout.add(new ArrayList<>(Arrays.asList(
+                    -1, -1, -1, -1, -1, 0, 0, -1, -1, -1, -1, -1, -1
+            )));
+            layout.add(new ArrayList<>(Arrays.asList(
+                    -1, -1, -1, -1, -1, -1, 0, -1, -1, -1, -1, -1, -1
+            )));
+        } else if (playerCount == 3) {
+            layout.add(new ArrayList<>(Arrays.asList(
+                    -1, -1, -1, -1, -1, -1, 0, -1, -1, -1, -1, -1, -1
+            )));
+            layout.add(new ArrayList<>(Arrays.asList(
+                    -1, -1, -1, -1, -1, 0, 0, -1, -1, -1, -1, -1, -1
+            )));
+            layout.add(new ArrayList<>(Arrays.asList(
+                    -1, -1, -1, -1, -1, 0, 0, 0, -1, -1, -1, -1, -1
+            )));
+            layout.add(new ArrayList<>(Arrays.asList(
+                    -1, -1, -1, -1, 0, 0, 0, 0, -1, -1, -1, -1, -1
+            )));
+            layout.add(new ArrayList<>(Arrays.asList(
+                    1, 1, 1, 1, 0, 0, 0, 0, 0, 3, 3, 3, 3
+            )));
+            layout.add(new ArrayList<>(Arrays.asList(
+                    1, 1, 1, 0, 0, 0, 0, 0, 0, 3, 3, 3, -1
+            )));
+            layout.add(new ArrayList<>(Arrays.asList(
+                    -1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 3, 3, -1
+            )));
+            layout.add(new ArrayList<>(Arrays.asList(
+                    -1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 3, -1, -1
+            )));
+            layout.add(new ArrayList<>(Arrays.asList(
+                    -1, -1, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1, -1
+            )));
+            layout.add(new ArrayList<>(Arrays.asList(
+                    -1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1, -1
+            )));
+            layout.add(new ArrayList<>(Arrays.asList(
+                    -1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1
+            )));
+            layout.add(new ArrayList<>(Arrays.asList(
+                    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1
+            )));
+            layout.add(new ArrayList<>(Arrays.asList(
+                    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+            )));
+            layout.add(new ArrayList<>(Arrays.asList(
+                    -1, -1, -1, -1, 2, 2, 2, 2, -1, -1, -1, -1, -1
+            )));
+            layout.add(new ArrayList<>(Arrays.asList(
+                    -1, -1, -1, -1, -1, 2, 2, 2, -1, -1, -1, -1, -1
+            )));
+            layout.add(new ArrayList<>(Arrays.asList(
+                    -1, -1, -1, -1, -1, 2, 2, -1, -1, -1, -1, -1, -1
+            )));
+            layout.add(new ArrayList<>(Arrays.asList(
+                    -1, -1, -1, -1, -1, -1, 2, -1, -1, -1, -1, -1, -1
+            )));
+        } else if (playerCount == 6) {
+            layout.add(new ArrayList<>(Arrays.asList(
+                    -1, -1, -1, -1, -1, -1, 6, -1, -1, -1, -1, -1, -1
+            )));
+            layout.add(new ArrayList<>(Arrays.asList(
+                    -1, -1, -1, -1, -1, 6, 6, -1, -1, -1, -1, -1, -1
+            )));
+            layout.add(new ArrayList<>(Arrays.asList(
+                    -1, -1, -1, -1, -1, 6, 6, 6, -1, -1, -1, -1, -1
+            )));
+            layout.add(new ArrayList<>(Arrays.asList(
+                    -1, -1, -1, -1, 6, 6, 6, 6, -1, -1, -1, -1, -1
+            )));
+            layout.add(new ArrayList<>(Arrays.asList(
+                    1, 1, 1, 1, 0, 0, 0, 0, 0, 4, 4, 4, 4
+            )));
+            layout.add(new ArrayList<>(Arrays.asList(
+                    1, 1, 1, 0, 0, 0, 0, 0, 0, 4, 4, 4, -1
+            )));
+            layout.add(new ArrayList<>(Arrays.asList(
+                    -1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 4, 4, -1
+            )));
+            layout.add(new ArrayList<>(Arrays.asList(
+                    -1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 4, -1, -1
+            )));
+            layout.add(new ArrayList<>(Arrays.asList(
+                    -1, -1, 0, 0, 0, 0, 0, 0, 0, 0, 0, -1, -1
+            )));
+            layout.add(new ArrayList<>(Arrays.asList(
+                    -1, 2, 0, 0, 0, 0, 0, 0, 0, 0, 3, -1, -1
+            )));
+            layout.add(new ArrayList<>(Arrays.asList(
+                    -1, 2, 2, 0, 0, 0, 0, 0, 0, 0, 3, 3, -1
+            )));
+            layout.add(new ArrayList<>(Arrays.asList(
+                    2, 2, 2, 0, 0, 0, 0, 0, 0, 3, 3, 3, -1
+            )));
+            layout.add(new ArrayList<>(Arrays.asList(
+                    2, 2, 2, 2, 0, 0, 0, 0, 0, 3, 3, 3, 3
+            )));
+            layout.add(new ArrayList<>(Arrays.asList(
+                    -1, -1, -1, -1, 5, 5, 5, 5, -1, -1, -1, -1, -1
+            )));
+            layout.add(new ArrayList<>(Arrays.asList(
+                    -1, -1, -1, -1, -1, 5, 5, 5, -1, -1, -1, -1, -1
+            )));
+            layout.add(new ArrayList<>(Arrays.asList(
+                    -1, -1, -1, -1, -1, 5, 5, -1, -1, -1, -1, -1, -1
+            )));
+            layout.add(new ArrayList<>(Arrays.asList(
+                    -1, -1, -1, -1, -1, -1, 5, -1, -1, -1, -1, -1, -1
             )));
         }
     }
